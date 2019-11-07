1a5b2e6c97082d27e45c733ace6a51a5d1306d48
ricardovolvox <ricardo.volvox@gmail.com>
2019-11-07 11:26:56 +0000

    Delete reddeConfig.js

--- reddeConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     receiveUrl : 'https://api.reddeonline.com/v1/receive/',
-     cashoutUrl : 'https://api.reddeonline.com/v1/cashout',
-     appID :0001 ,//Replace 0001 with your app ID
-     apiKey : '' //Enter your Api Key here
-}
